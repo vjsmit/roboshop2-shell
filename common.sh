b35af521008fc5df7ce01c6ef903deeee88f062c
vjsmit <vjsmit97@gmail.com>
2024-02-18 00:01:47 +0530

    DRY

--- common.sh
+++ common.sh
@@ -1,6 +1,11 @@
 color="\e[35m"
 nocolor="\e[0m"
 logfile="/tmp/roboshop.log"
+user_id=$(id -u)
+if [ $user_id -ne 0 ]; then
+  echo Script should run as root user, exiting script
+  exit 1
+fi
 
 stat_check() {
   if [ $1 -eq 0 ]; then
