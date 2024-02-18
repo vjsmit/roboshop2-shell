5441548e1d0a6eb8e25092c55df8359733c7bfb6
vjsmit <vjsmit97@gmail.com>
2024-02-18 20:43:17 +0530

    Setup MongoDB

--- mongodb.sh
+++ mongodb.sh
@@ -1,19 +1,19 @@
 source common.sh
 
 echo -e "${color}Setup repo file${nocolor}"
-cp /home/centos/roboshop2-shell /etc/yum.repos.d/mongo.repo     &>>${log_file}
+cp /home/centos/roboshop2-shell /etc/yum.repos.d/mongo.repo     &>>${logfile}
 stat_check $?
 
 echo -e "${color}Install MongoDB${nocolor}"
-dnf install mongodb-org -y    &>>${log_file}
+dnf install mongodb-org -y    &>>${logfile}
 stat_check $?
 
 echo "${color}Update listen address${nocolor}"
-sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/mongod.conf   &>>${log_file}
+sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/mongod.conf   &>>${logfile}
 stat_check $?
 
 echo "${color}Start MongoD service${nocolor}"
-systemctl enable mongod   &>>${log_file}
-systemctl restart mongod    &>>${log_file}
+systemctl enable mongod   &>>${logfile}
+systemctl restart mongod    &>>${logfile}
 stat_check $?
 
