c1322ce0f030ceb3a8b18e6bd9406f55d7e3f039
vjsmit <vjsmit97@gmail.com>
2024-02-18 22:27:09 +0530

    setup redis

--- redis.sh
+++ redis.sh
@@ -1,15 +1,22 @@
+source common.sh
+
 echo -e "${color}Setup Redis repo${nocolor}"
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${logfile}
+stat_check $?
 
 echo -e "${color}Enable Redis 6.2${nocolor}"
-dnf module enable redis:remi-6.2 -y
+dnf module enable redis:remi-6.2 -y   &>>${logfile}
+stat_check $?
 
 echo -e "${color}Install Redis${nocolor}"
-dnf install redis -y
+dnf install redis -y    &>>${logfile}
+stat_check $?
 
 echo -e "${color}Update listen address${nocolor}"
-sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/redis.conf /etc/redis/redis.conf
+sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/redis.conf /etc/redis/redis.conf    &>>${logfile}
+stat_check $?
 
 echo -e "${color}Enable Redis Service${nocolor}"
-systemctl enable redis
-systemctl restart redis
+systemctl enable redis    &>>${logfile}
+systemctl restart redis   &>>${logfile}
+stat_check $?
