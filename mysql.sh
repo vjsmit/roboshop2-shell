4d3fa9de7fb7f2c93556f943cb0024bed7ca7ada
vjsmit <vjsmit97@gmail.com>
2024-02-20 21:21:47 +0530

    modify user

--- mysql.sh
+++ mysql.sh
@@ -8,16 +8,21 @@ fi
 
 echo -e "${color}Disable MySQL 8 version${nocolor}"
 dnf module disable mysql -y     &>>{logfile}
+stat_check $?
 
 echo -e "${color}Setup the MySQL5.7 repo file${nocolor}"
 cp /home/centos/roboshop2-shell/mysql.repo /etc/yum.repos.d/mysql.repo   &>>{logfile}
+stat_check $?
 
 echo -e "${color}Install MySQL Server${nocolor}"
 dnf install mysql-community-server -y   &>>{logfile}
+stat_check $?
 
 echo -e "${color}Start MySQL Service${nocolor}"
 systemctl enable mysqld   &>>{logfile}
 systemctl restart mysqld    &>>{logfile}
+stat_check $?
 
 echo -e "${color}Change the default root password${nocolor}"
 mysql_secure_installation --set-root-pass ${mysql_pwd}    &>>{logfile}
+stat_check $?
