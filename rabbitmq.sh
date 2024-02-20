c3af8d0fab6945fcdd09189e8705695f690e8509
vjsmit <vjsmit97@gmail.com>
2024-02-20 21:23:50 +0530

    modify user

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -7,24 +7,24 @@ if [ -z  "$rabbitmq_pwd" ]; then
     exit 1
 fi
 
-echo - e "${color}Configure Erlang YUM Repos${nocolor}"
+echo -e "${color}Configure Erlang YUM Repos${nocolor}"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash     &>>{logfile}
 stat_check $?
 
-echo - e "${color}Configure rabbitmq YUM Repos${nocolor}"
+echo -e "${color}Configure rabbitmq YUM Repos${nocolor}"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash    &>>{logfile}
 stat_check $?
 
-echo - e "${color}Install RabbitMQ${nocolor}"
+echo -e "${color}Install RabbitMQ${nocolor}"
 dnf install rabbitmq-server -y    &>>{logfile}
 stat_check $?
 
-echo - e "${color}Start RabbitMQ Service${nocolor}"
+echo -e "${color}Start RabbitMQ Service${nocolor}"
 systemctl enable rabbitmq-server    &>>{logfile}
 systemctl restart rabbitmq-server   &>>{logfile}
 stat_check $?
 
-echo - e "${color}Create one user for the app${nocolor}"
+echo -e "${color}Create one user for the app${nocolor}"
 rabbitmqctl add_user roboshop ${rabbitmq_pwd}   &>>{logfile}
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"    &>>{logfile}
 stat_check $?
